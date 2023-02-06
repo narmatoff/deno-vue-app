0f55ee2f886fd0feeb755b77813e0dbf8139b1d6
narmatoff <narmatoff@ya.ru>
2023-02-06 11:51:59 +0300

    create vue deno app

---
+++ src/components/Dinosaur.vue
@@ -0,0 +1,16 @@
+<script>
+import { store } from '../store.js';
+export default {
+  data() {
+    return {
+      store
+    }
+  }
+}
+</script>
+
+<template>
+  Name: {{ store.dinosaur.name }}
+  <br />
+  Description: {{ store.dinosaur.description }}
+</template>
